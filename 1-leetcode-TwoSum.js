4f4cda25da9efb945a3dd09d21ed6d4e0f48b6bb
Rupali <rupali.yadav0@gmail.com>
2026-02-27 18:57:23 +0530

    217-contains duplicate

--- 1-leetcode-TwoSum.js
+++ 1-leetcode-TwoSum.js
@@ -1,6 +1,21 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
 const twoSum = (nums, target) => {
     const mp = new Map();
 
+    // loop on the array
     for (let i = 0; i < nums.length; i++) {
         const diff = target - nums[i];
 
---
+++ 217-ContainsDuplicate.js
@@ -0,0 +1,38 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Explanation:
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+// Input: nums = [1,2,3,4]
+
+// Output: false
+// Explanation:
+// All elements are distinct.
+
+// option 1
+const containsDuplicate = (nums) => {
+
+    const seenNumbers = new Set();
+
+    for (const num of nums) {
+        if (seenNumbers.has(num)) return true;
+        else seenNumbers.add(num);
+    }
+
+    return false;
+}
+
+// option 2
+const containsDuplicate2 = (nums) => {
+
+    // duplicates are automatically ignored in a set.
+    return nums.length !== new Set(nums);
+
+}
